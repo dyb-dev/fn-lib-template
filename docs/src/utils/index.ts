a6e333da033d2566fdc278f88ff0fab1139d6596
dyb-dev <2737993954@qq.com>
2024-10-16 01:13:57 +0800

    ✨ feat: 新增工具函数

--- docs/src/utils/index.ts
+++ docs/src/utils/index.ts
@@ -1,2 +1,13 @@
-// 导出 url 相关方法
+/*
+ * @Author: dyb-dev
+ * @Date: 2024-08-18 01:03:56
+ * @LastEditors: dyb-dev
+ * @LastEditTime: 2024-10-15 23:48:53
+ * @FilePath: /fn-lib-template/docs/src/utils/index.ts
+ * @Description: 工具函数模块
+ */
+
+/** 导出环境相关工具函数 */
+export * from "./env"
+/** 导出url相关工具函数 */
 export * from "./url"
