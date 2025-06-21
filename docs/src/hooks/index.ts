e553b81f571d719bef25b7a225c506be69e557dc
dyb-dev <2737993954@qq.com>
2025-06-21 14:39:17 +0800

    ✨ feat: 新增 styles mixin 和 hooks 函数

---
+++ docs/src/hooks/index.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: dyb-dev
+ * @Date: 2025-06-21 14:31:53
+ * @LastEditors: dyb-dev
+ * @LastEditTime: 2025-06-21 14:32:02
+ * @FilePath: /fn-lib-template/docs/src/hooks/index.ts
+ * @Description: hook模块
+ */
+
+/** 导出滚动控制相关hooks函数 */
+export * from "./scroll"
